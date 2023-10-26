279c462e2ec4fbd9ba3f17a55818d57140e9c300
Ivan <iv4n9f@gmail.com>
2023-10-26 19:52:41 +0200

    1.001

--- install.sh
+++ install.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
+dir=$(pwd)
+
 cd ~
+
 sudo pacman -Syu
-sudo pacman -S vim git libxcb xcb-util xcb-util-wm xcb-util-keysyms gdm xorg xorg-xinit polybar --noconfirm
+sudo pacman -S vim git libxcb xcb-util xcb-util-wm xcb-util-keysyms lightdm lightdm-gtk-greeter xorg xorg-xinit polybar xterm rofi feh --noconfirm
+
 git clone https://github.com/baskerville/bspwm.git
 git clone https://github.com/baskerville/sxhkd.git
+
+mkdir -p ~/.config/{bspwm,sxhkd,polybar}
+mkdir ~/.config/bspwm/scripts
+mkdir ~/pictures ~/pictures/wallpaper.jpg
+
 cd bspwm && make && sudo make install
 cd ../sxhkd && make && sudo make install
-mkdir -p ~/.config/{bspwm,sxhkd,polybar}
-cd ../bspwm/examples
-cp bspwmrc ~/.config/bspwm/
-cp sxhkdrc ~/.config/sxhkd/
-chmod u+x ~/.config/bspwm/bspwmrc
+
+cp $dir/bspwmrc ~/.config/bspwm/
+cp $dir/bspwmrc_resize ~/.config/bspwm/scripts
+cp $dir/sxhkdrc ~/.config/sxhkd/
+cp $dir/launch.sh ~/.config/polybar/
+cp $dir/wallpaper.jpg
+
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
-echo "exec bspwm" >> ~/.xinitrc
 cp /etc/polybar/config.ini ~/.config/polybar/
-sudo systemctl enable gdm
+
+echo "exec bspwm" >> ~/.xinitrc
+
+chmod u+x ~/.config/bspwm/bspwmrc
+chmod +x ~/.config/polybar/launch.sh
+
+sudo systemctl enable lightdm
 sudo reboot

15efb99ca3e5ed93e0fdd3a11c23860a46b40667
Ivan <iv4n9f@gmail.com>
2023-10-28 15:50:09 +0200

    1.06
    Clean and removes

--- install.sh
+++ install.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
 dir=$(pwd)
-
 cd ~
-
 sudo pacman -Syu
-sudo pacman -S vim git libxcb xcb-util xcb-util-wm xcb-util-keysyms gdm xorg xorg-xinit polybar xterm rofi feh tmux chromium --noconfirm
-
+sudo pacman -S vim git libxcb xcb-util xcb-util-wm xcb-util-keysyms gdm xorg xorg-xinit polybar xterm rofi feh tmux chromium open-vm-tools net-tools --noconfirm
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 git clone https://github.com/baskerville/bspwm.git
 git clone https://github.com/baskerville/sxhkd.git
-
 mkdir -p ~/.config/{bspwm,sxhkd,polybar}
 mkdir ~/.config/bspwm/scripts
 mkdir ~/pictures 
-
 cd bspwm && make && sudo make install
 cd ../sxhkd && make && sudo make install
-
 cp $dir/bspwmrc ~/.config/bspwm/
 cp $dir/bspwmrc_resize ~/.config/bspwm/scripts
 cp $dir/sxhkdrc ~/.config/sxhkd/
@@ -24,16 +19,10 @@ cp $dir/launch.sh ~/.config/polybar/
 cp $dir/config.ini ~/.config/polybar/
 cp $dir/wallpaper.jpg ~/pictures/wallpaper.jpg
 cp $dir/wallpaper.jpg
-
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
-
 echo "exec bspwm" >> ~/.xinitrc
-
 chmod u+x ~/.config/bspwm/bspwmrc
 chmod +x ~/.config/polybar/launch.sh
-
-# Optional
-
 cd ~
 git clone https://github.com/lr-tech/rofi-themes-collection.git
 cd rofi-themes-collection
@@ -49,6 +38,15 @@ mkdir -p ~/.local/share/fonts
 cp *.ttf ~/.local/share/fonts/.
 cp -r $dir/scripts ~/.config/polybar/
 chmod u+x ~/.config/polybar/scripts/*
-
+cd ~
+sudo rm -r bspwm fonts rofi-themes-collection sxhkd archDefault yay
+yay -Y --gendb
+yay -Syu
+yay -Syu --devel
+yay -S snapd --noconfirm
+sudo systemctl enable snapd && sudo systemctl start snapd
 sudo systemctl enable gdm
-
+sudo systemctl enable vmtoolsd.service
+sudo ln -s /var/lib/snapd/snap /snap
+sudo snap install --classic code
+sudo ln -s /snap/bin/code /bin/code
